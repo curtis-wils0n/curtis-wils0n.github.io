1db4fc46f1261a2df73dcb4334e0a0db002d4d0c
curtis-wils0n <curtis.wils0n@protonmail.com>
2023-01-03 16:55:35 -0700

    Include loading gif

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useImagePreloader from "../hooks/useImagePreloader";
 import { container, logoContainer, logo, loadingLogo } from "../styles/profile.module.css";
+import spinner from '../images/spinner.gif'
 
 const profileImage = ["https://github.com/curtis-wils0n/curtis-wils0n.github.io/blob/feature/front-page/src/images/profile.png?raw=true"];
 
@@ -17,8 +18,9 @@ const Profile = () => {
               src={ profileImage }
               alt="Profile"
             />
-          : <span
+          : <img
               className={ loadingLogo }
+              src={spinner}
             />
         }
       </div>
